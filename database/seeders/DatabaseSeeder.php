0a06d74d2375c134f6807d0a89af7131a205293d
angelbendgr8-dev <63807149+angelbendgr8-dev@users.noreply.github.com>
2022-07-14 15:17:42 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'name' => 'MotoringApp Admin',
             'email' => 'admin@motoringapp.com',
             'password' => Hash::make('password'),
